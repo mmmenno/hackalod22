bf4dcfaed48961b518ed06723d710e2ef5c71f68
PHPetra <petra@dreis.nl>
2022-11-04 19:20:33 +0100

    Add files en directorie

---
+++ app2/gebieden/index.php
@@ -0,0 +1,15 @@
+<?php
+
+# Alle natuurgebieden, kies gebied, etc
+
+?>
+<html>
+<head>
+    <title>HetWildeNL - Collectie flora en fauna</title>
+    <link href="/style.css" rel="stylesheet">
+</head>
+<body>
+<h1>Collectie flora en fauna</h1>
+<h2>Natuurgebieden
+</body>
+</html>
---
+++ app2/soorten/index.php
@@ -0,0 +1,14 @@
+<?php
+
+# Pagina voor alle soorten (vogels - bomen )
+?>
+<html>
+<head>
+    <title>HetWildeNL - Collectie flora en fauna</title>
+    <link href="/style.css" rel="stylesheet">
+</head>
+<body>
+<h1>Collectie flora en fauna</h1>
+<h2>Soorten
+</body>
+</html>
