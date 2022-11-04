ad4f6a1a0340c07c345c13efaea7f84e7f43b494
Arnout Engelen <arnout@bzzt.net>
2022-11-04 23:16:12 +0100

    Don't produce sounds for taxons

--- app2/soorten/soort.php
+++ app2/soorten/soort.php
@@ -11,7 +11,9 @@ foreach (uvaImages($taxonId) as $row) {
     print("<img src='$small_url' height='300'/>");
 }
 foreach (commonsImages($taxonId) as $row) {
-    $full_url = $row['image']['value'];
-    print("<img src='$full_url?width=300' height='300'/>");
+    $file_entity_url = $row['file']['value'];
+    $full_image_url = $row['image']['value'];
+    print("<!-- $file_entity_url -->");
+    print("<img src='$full_image_url?width=300' height='300'/>");
 }
 ?>
