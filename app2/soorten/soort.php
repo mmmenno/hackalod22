97807f3932b558811be645ea943ddba1d1ee0cca
PHPetra <petra@dreis.nl>
2022-11-04 23:33:37 +0100

    Add nozeman birds

--- app2/soorten/soort.php
+++ app2/soorten/soort.php
@@ -4,7 +4,12 @@ include("uva.php");
 include("commons.php");
 
 $taxonId = $_GET["taxonId"];
-
+foreach (commonsImages($taxonId, true) as $row) {
+    $file_entity_url = $row['file']['value'];
+    $full_image_url = $row['image']['value'];
+    print("<!-- $file_entity_url -->");
+    print("<img src='$full_image_url?width=300' height='300'/>");
+}
 foreach (uvaImages($taxonId) as $row) {
     $full_url = $row['imageURL']['value'];
     $small_url = str_replace("full/full", "full/300,", $full_url);
@@ -16,4 +21,3 @@ foreach (commonsImages($taxonId) as $row) {
     print("<!-- $file_entity_url -->");
     print("<img src='$full_image_url?width=300' height='300'/>");
 }
-?>
