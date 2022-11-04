37b8a4cd72baa1cff2f16f584933ca62bdf151db
Arnout Engelen <arnout@bzzt.net>
2022-11-04 22:04:20 +0100

    pics from wikimedia commons

--- app2/soorten/soort.php
+++ app2/soorten/soort.php
@@ -1,6 +1,7 @@
 <?php
 
 include("uva.php");
+include("commons.php");
 
 $taxonId = $_GET["taxonId"];
 
@@ -8,5 +9,9 @@ foreach (uvaImages($taxonId) as $row) {
     $full_url = $row['imageURL']['value'];
     $small_url = str_replace("full/full", "full/300,", $full_url);
     print("<img src='$small_url' height='300'/>");
+}
+foreach (commonsImages($taxonId) as $row) {
+    $full_url = $row['image']['value'];
+    print("<img src='$full_url' height='300'/>");
 }
 ?>
