756d9a28c5b86e09322a6fccc8151483c8b4e62a
Arnout Engelen <arnout@bzzt.net>
2022-11-05 02:33:52 +0100

    Show all images in one loop

--- app2/soorten/soort.php
+++ app2/soorten/soort.php
@@ -12,24 +12,14 @@ include("../../app3/individuals_query.php");
 
 $taxonId = $_GET["taxonId"];
 
-print("<!-- results from Nozeman -->\n");
-foreach (commonsImages($taxonId, true) as $row) {
-    print("<!-- Nozeman ${row['entity']} -->");
-    print("<img src='${row['image']}' height='300'/>");
-}
-print("<!-- results from uva -->\n");
-foreach (uvaImages($taxonId) as $row) {
-    print("<!-- uva -->");
-    print("<img src='${row['image']}' height='300'/>");
-}
-print("<!-- results from dijkshoorn -->\n");
-foreach (dijkshoornImages($taxonId) as $row) {
-    print("<!-- dijkshoorn -->");
-    print("<img src='${row['image']}' height='300'/>");
-}
-print("<!-- other results from other commons -->\n");
-foreach (commonsImages($taxonId) as $row) {
-    print("<!-- Commons ${row['entity']} -->");
+$images = array_merge(
+    commonsImages($taxonId, true),
+    uvaImages($taxonId),
+    dijkshoornImages($taxonId),
+    commonsImages($taxonId)
+);
+
+foreach ($images as $row) {
     print("<img src='${row['image']}' height='300'/>");
 }
 if (!empty(queryIndividuals($taxonId))) {
